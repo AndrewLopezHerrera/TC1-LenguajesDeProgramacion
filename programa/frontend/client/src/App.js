6e363afd9211b28811caf90d81c31976ce441f53
AndrewLopezHerrera <80350719+AndrewLopezHerrera@users.noreply.github.com>
2024-02-15 21:41:42 -0600

    Creación cliente y servidor

---
+++ programa/frontend/client/src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+
+function App() {
+  return (
+    <body>
+      <div class = "titulo">Ahorcado</div>
+      <div class = "PantallaJuego">
+        <div>Jugadores</div>
+        <div class = "IngreseNombre">Ingresar nombre
+          <div>
+            <div>
+              Jugador 1
+            </div>
+            <div>
+              Ingresar nombre
+            </div>
+          </div>
+          <div>
+            <div>
+              Jugador 2
+            </div>
+            <div>
+              Ingresar nombre
+            </div>
+          </div>
+        </div>
+        <div>Jugar</div>
+      </div>
+    </body>
+  );
+}
+
+export default App;
---
+++ programa/frontend/client/src/Gameplay.js
@@ -0,0 +1,16 @@
+import "./Gameplay.css"
+
+function SeeGameplay(){
+    return (
+        <body>
+        <div>Ahorcado</div>
+        <div class = "PantallaJuego">
+            <div>Puntaje</div>
+            <div>Imagen</div>
+            <div>Controles</div>
+        </div>
+        </body>
+    )
+}
+
+export default SeeGameplay;
