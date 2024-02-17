823cc2f4487963b0f5fac484caf8dd34971406d8
AndrewLopezHerrera <80350719+AndrewLopezHerrera@users.noreply.github.com>
2024-02-17 15:43:27 -0600

    Se ha avanzado el frontend

--- programa/frontend/client/src/App.js
+++ programa/frontend/client/src/App.js
@@ -1,32 +1,119 @@
 import './App.css';
 
-function App() {
+function InsertarTitulo(){
   return (
-    <body>
-      <div class = "titulo">Ahorcado</div>
-      <div class = "PantallaJuego">
-        <div>Jugadores</div>
-        <div class = "IngreseNombre">Ingresar nombre
-          <div>
-            <div>
-              Jugador 1
-            </div>
-            <div>
-              Ingresar nombre
-            </div>
-          </div>
-          <div>
-            <div>
-              Jugador 2
-            </div>
-            <div>
-              Ingresar nombre
-            </div>
-          </div>
-        </div>
-        <div>Jugar</div>
+    <div class = "titulo">Ahorcado</div>
+  );
+}
+
+function InsertarCuerpo(){
+  return (
+    <div class = "ContenedorPantalla">
+      <InsertarTitulo />
+      <AgregarPantallaJuego />
+      <InsertarHistorialJuegos />
+    </div>
+  );
+}
+
+function InsertarHistorialJuegos(){
+  return (
+    <div class = "PantallaJuego">
+      <h1 class = "TituloHistorial">Historial de Juegos</h1>
+      <table id = "Historial" class = "TablaHistorial">
+        <InsertarEncabezado />
+
+      </table>
+    </div>
+  );
+}
+
+function InsertarEncabezado(){
+  return (
+    <thead>
+      <tr>
+        <td>Número de Juego</td>
+        <td>Jugador 1</td>
+        <td>Jugador 2</td>
+        <td>Resultado</td>
+        <td>Ganador</td>
+      </tr>
+    </thead>
+  );
+}
+
+function AgregarPantallaJuego(){
+  return (
+    <div class = "PantallaJuego" style={{display: "none"}}>
+        <div style={{height: "50%"}} />
+        <AgregarRegistradoresJugadores />
+        <AgregarControlesInicio />
+    </div>
+  );
+}
+
+function AgregarRegistradoresJugadores(){
+  return(
+    <div class = "IngreseNombre">
+      <AgregarRegistradorJugadorUno />
+      <div class = "EspacioJugadores"></div>
+      <AgregarRegistradorJugadorDos />
+    </div>
+  );
+}
+
+function AgregarRegistradorJugadorUno(){
+  return(
+    <div class = "NombreJugadores">
+      <div class = "TituloJugador">
+        Jugador 1
       </div>
-    </body>
+      <div class = "SeccionEntradaNombres">
+        <input></input>
+      </div>
+      <div id = "MensajeJugadorUno"></div>
+      <div class = "ContenedorBotonAceptarNombre">
+        <button class = "BotonAceptarNombre">Aceptar</button>
+      </div>
+    </div>
+  );
+}
+
+function AgregarRegistradorJugadorDos(){
+  return(
+    <div class = "NombreJugadores">
+      <div class = "TituloJugador">
+        Jugador 2
+      </div>
+      <div class = "SeccionEntradaNombres">
+        <input></input>
+      </div>
+      <div id = "MensajeJugadorDos"></div>
+      <div class = "ContenedorBotonAceptarNombre">
+        <button class = "BotonAceptarNombre">Aceptar</button>
+      </div>
+    </div>
+  );
+}
+
+function AgregarControlesInicio(){
+  return(
+    <div class = "contenedorBotonesInicio">
+      <div class = "ContenedorBotonInicio">
+        <button class = "BotonesInicio">Jugar</button>
+      </div>
+      <div class = "ContenedorBotonInicio">
+        <button class = "BotonesInicio">Ver historial de juego</button>
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  document.getElementById("root").style.width = "100%";
+  document.getElementById("root").style.height = "100%"
+  return (
+    <InsertarCuerpo />
   );
 }
 
